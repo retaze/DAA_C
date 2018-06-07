c3f4d320d30b4490b833630d48caf030154b90af
Lars Gruber <Lars Gruber@AD-FIAE10>
2018-06-07 12:13:33 +0200

    Erste Projekte zum Test

---
+++ Hello C++/helloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+ * helloWorld.cpp
+ *
+ *  Created on: 07.06.2018
+ *      Author: Lars Gruber
+ */
+
+#include "helloWorld.h"
+
+helloWorld::helloWorld() {
+	// TODO Auto-generated constructor stub
+
+}
+
+helloWorld::~helloWorld() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Hello C++/helloWorld.h
@@ -0,0 +1,17 @@
+/*
+ * helloWorld.h
+ *
+ *  Created on: 07.06.2018
+ *      Author: Lars Gruber
+ */
+
+#ifndef HELLOWORLD_H_
+#define HELLOWORLD_H_
+
+class helloWorld {
+public:
+	helloWorld();
+	virtual ~helloWorld();
+};
+
+#endif /* HELLOWORLD_H_ */
