ec09bdf8727c385f1768fae199acdb379bad8112
Lars Gruber <Lars Gruber@AD-FIAE10>
2018-06-18 14:59:23 +0200

    1

---
+++ Hello C++/HelloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: 18.06.2018
+ *      Author: Lars Gruber
+ */
+
+#include "HelloWorld.h"
+
+#include <iostream>
+
+int main()
+{
+	const char* std::cout;
+    std::cout << "Hello, World!";
+    return 0;
+}
--- Hello C++/HelloWorld.h
+++ Hello C++/HelloWorld.h
@@ -1,17 +1,17 @@
 /*
- * helloWorld.h
+ * HelloWorld.h
  *
- *  Created on: 07.06.2018
+ *  Created on: 18.06.2018
  *      Author: Lars Gruber
  */
 
 #ifndef HELLOWORLD_H_
 #define HELLOWORLD_H_
 
-class helloWorld {
+class HelloWorld {
 public:
-	helloWorld();
-	virtual ~helloWorld();
+	HelloWorld();
+	virtual ~HelloWorld();
 };
 
 #endif /* HELLOWORLD_H_ */
